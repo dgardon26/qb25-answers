4384b7bdf949fe9efba74dfcf02d26aca27d1c50
Danielle <dgardon1@jh.edu>
2025-09-04 16:55:07 -0400

    updated script

--- miniproject-codon-usage/codon-usage.py
+++ miniproject-codon-usage/codon-usage.py
@@ -3,12 +3,31 @@
 import sys
 import fasta
 
-my_file = open(sys.argv[1])
-codons = fasta.FASTAReader(my_file)
+import codons
 
-for ident, sequence in codons:
+# print(codons.forward)
+
+
+aas = {}
+
+my_file = open(sys.argv[1])
+codons_reader = fasta.FASTAReader(my_file)
+
+for ident, sequence in codons_reader:
     
     for i in range(0, len(sequence), 3):
         # print(ident)
         codon = sequence[i:i+3]
         # print(codon)
+
+        current_aa = codons.forward.get(codon)
+        
+
+        if codon not in aas.keys():
+            aas.setdefault(current_aa, 0)
+            aas[current_aa] += 1
+        
+        else: 
+            aas[current_aa] += 1
+
+print(aas)
