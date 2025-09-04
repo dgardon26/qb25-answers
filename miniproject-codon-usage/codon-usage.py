430d8392f930a95bba189d0bac03d94146d1aff8
Danielle <dgardon1@jh.edu>
2025-09-04 17:06:01 -0400

    Updated with comparison function

--- miniproject-codon-usage/codon-usage.py
+++ miniproject-codon-usage/codon-usage.py
@@ -8,10 +8,13 @@ import codons
 # print(codons.forward)
 
 
-aas = {}
+aas1 = {}
+aas2 = {}
 
 my_file = open(sys.argv[1])
+my_file2 = open(sys.argv[2])
 codons_reader = fasta.FASTAReader(my_file)
+codons2_reader = fasta.FASTAReader(my_file2)
 
 for ident, sequence in codons_reader:
     
@@ -23,11 +26,34 @@ for ident, sequence in codons_reader:
         current_aa = codons.forward.get(codon)
         
 
-        if codon not in aas.keys():
-            aas.setdefault(current_aa, 0)
-            aas[current_aa] += 1
+        if codon not in aas1.keys():
+            aas1.setdefault(current_aa, 0)
+            aas1[current_aa] += 1
         
         else: 
-            aas[current_aa] += 1
+            aas1[current_aa] += 1
 
-print(aas)
+
+
+for ident, sequence in codons2_reader:
+
+    for i in range(0, len(sequence), 3):
+        codon = sequence[i:i+3]
+
+        current_aa2 = codons.forward.get(codon)
+
+        if codon not in aas2.keys():
+            aas2.setdefault(current_aa2, 0)
+            aas2[current_aa2] += 1
+
+        else: 
+            aas2[current_aa2] += 1
+
+table_list = list(codons.reverse.keys())
+table_list.sort()
+print(table_list)
+
+for letter in table_list:
+    count1 = aas1.get(letter)
+    count2 = aas2.get(letter)
+    print(f"{letter}\t{count1}\t{count2}")
