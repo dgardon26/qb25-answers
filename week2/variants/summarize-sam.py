c3ba411268cb4863c0ed1da5e173be0f15b8062c
Danielle <dgardon1@jh.edu>
2025-09-19 12:23:19 -0400

    Python script for counting alignments and mismatches

---
+++ week2/variants/summarize-sam.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys
+
+my_sam_file = open(sys.argv[1])
+sam_reads = {}
+NM_count = {}
+
+for line in my_sam_file:
+
+    if line.startswith("@"):
+        continue
+    
+    columns = line.split('\t')
+
+    chromosome = columns[2]
+
+    if chromosome not in sam_reads:
+        sam_reads[chromosome] = 0
+        sam_reads[chromosome] += 1
+    
+    else:
+        sam_reads[chromosome] += 1
+
+
+    for i in columns:
+
+        if i.startswith("NM"):
+        
+            if int(i[5:]) not in NM_count:
+                NM_count[int(i[5:])] = 0
+                NM_count[int(i[5:])] += 1
+        
+            else:
+                NM_count[int(i[5:])] += 1
+
+
+for i in sam_reads:
+    print(i + " " + str(sam_reads[i]))
+
+sorted_NM_counts = sorted(NM_count.items())
+print(sorted_NM_counts)
+
