e5da9451c74d206e7ae234dab989d73988a4760d
Danielle <dgardon1@jh.edu>
2025-10-26 14:02:20 -0400

    Fixed Python script for Exercise 2

--- week3/BYxRM_bam/ex2.py
+++ week3/BYxRM_bam/ex2.py
@@ -9,20 +9,31 @@ for line in open("biallelic.vcf"):
         continue
     fields = line.rstrip('\n').split('\t')
     info = fields[7]
-    
-    separated_info = info.split(";")
-    
-    for items in separated_info:
-        if items.startswith("AF"):
-            my_file.write(items[3:] + "\n")
-        
-        if items.startswith("DP"):
+    info_items = info.split(";")
+    for thing in info_items:
+        if thing.startswith("AF"):
+            my_file.write(thing[3:] + "\n")
+    i = 9
+    for i in fields: # GT:GQ:DP:AD:RO:QR:AO:QA:GL so want 3rd item in list (depth)
+        info = fields[9]
+        separated_info = info.split("\t")
+        for entry in separated_info:
+            separated_entry = entry.split(":")
+            my_other_file.write(separated_entry[2] + "\n")
 
-            if items.startswith("DPB"):
-                continue
-            if items.startswith("DPRA"):
-                continue
-            my_other_file.write(items[3:] + "\n")
+            
+    
+   # for items in separated_info:
+      #  if items.startswith("AF"):
+          #  my_file.write(items[3:] + "\n")
+        
+       # if items.startswith("DP"):
+
+         #   if items.startswith("DPB"):
+            #    continue
+          #  if items.startswith("DPRA"):
+            #    continue
+          #  my_other_file.write(items[3:] + "\n")
 
      
 
