01d6f7628a49d5a8453ae6a98ec1a0f1bf4f6210
Danielle <dgardon1@jh.edu>
2025-10-28 15:54:19 -0400

    Fixed issue with Python script

--- week3/BYxRM_bam/ex2.py
+++ week3/BYxRM_bam/ex2.py
@@ -13,14 +13,17 @@ for line in open("biallelic.vcf"):
     for thing in info_items:
         if thing.startswith("AF"):
             my_file.write(thing[3:] + "\n")
-    i = 9
-    for i in fields: # GT:GQ:DP:AD:RO:QR:AO:QA:GL so want 3rd item in list (depth)
-        info = fields[9]
-        separated_info = info.split("\t")
-        for entry in separated_info:
-            separated_entry = entry.split(":")
-            my_other_file.write(separated_entry[2] + "\n")
-
+   # i = 9
+   #for i in fields: # GT:GQ:DP:AD:RO:QR:AO:QA:GL so want 3rd item in list (depth)
+      #  info = fields[i]
+      #  separated_info = info.split("\t")
+       # for entry in separated_info:
+       #     separated_entry = entry.split(":")
+        #    my_other_file.write(separated_entry[2] + "\n")
+    for i in fields[9:]: #for every column (sample) in columns 10 to the end 
+      items = i.split(":") #split the GT:GQ:DP:AD:RO:QR:AO:QA:GL by :
+      dp = items[2] #take the DP 
+      my_other_file.write(dp + "\n") #put it in the file
             
     
    # for items in separated_info:
